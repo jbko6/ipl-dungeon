9904d7743838a0e3017f30a56c51f0153e7d55b4
Jonah Kowal <jonahkowal@duck.com>
2026-05-18 10:43:40 -0700

    try alias

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import { DB_URL, AUTH_TOKEN } from "$env/static/private";
 
